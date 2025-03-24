0f19c5f17407f9c2265cfea2bb62231bc30d6292
Alex Gorbunov <alextk37@yandex.ru>
2025-03-24 10:30:58 +0300

    fix munu js

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const burgerIcon = document.getElementById("burger-icon");
+  const burgerIcon = document.getElementById("burger-icon"); // <object> с SVG
   const dropdown = document.getElementById("dropdown");
   const closeMenu = document.getElementById("close-menu");
 
+  // Обработка загрузки SVG внутри <object>
   burgerIcon.addEventListener("load", function () {
     const svgDoc = burgerIcon.contentDocument;
     const svgElement = svgDoc.querySelector("svg");
@@ -15,10 +16,12 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 
+  // Закрытие меню по клику на кнопку закрытия
   closeMenu.addEventListener("click", function () {
     dropdown.classList.remove("active");
   });
 
+  // Закрытие меню по клику вне dropdown
   document.addEventListener("click", function (event) {
     if (!dropdown.contains(event.target)) {
       dropdown.classList.remove("active");
